9b3bc639f62fb366845d9c849a70faa4d2834469
aliakseyeuraman <aliakseyeu.raman@gmail.com>
2017-02-04 23:22:55 +0300

    codestyle was fixed

--- src/igorhmm/currenttime/EntryPoint.java
+++ src/igorhmm/currenttime/EntryPoint.java
@@ -4,8 +4,8 @@ package igorhmm.currenttime;
  * Entry point to software to get current time,
  * No input parameters from CL are used
  */
-public class EntryPoint{
-	public static void main(String[] args){
-		System.out.println("Current time is " + CurrentTime.getTime("HH:mm:ss")); //TODO: make time format as constant value
-	}
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println("Current time is " + CurrentTime.getTime("HH:mm:ss"));
+    }
 }
